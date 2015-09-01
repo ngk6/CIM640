e4e52c51a6d5dd03719d8afd89bc8838e7181604
Natalie Kaminsky <kaminskynat@gmail.com>
2015-09-01 08:52:09 -0400

    working on p5 tutorial

--- practice/sketch.js
+++ practice/sketch.js
@@ -1,7 +1,13 @@
 function setup() {
-  
+  createCanvas(640, 480);
 }
 
 function draw() {
+  if (mouseIsPressed) {
+    fill(random(254));
+  } else {
+    fill(random(254),random(254),random(254));
+  }
+  ellipse(mouseX, mouseY, 80, 80);
   
 }
