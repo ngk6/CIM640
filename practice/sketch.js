d55ca9fccce8ba0b4358b52042db28ea8d6875e8
Natalie Kaminsky <kaminskynat@gmail.com>
2015-09-02 19:30:09 -0400

    function addOrange

--- practice/sketch.js
+++ practice/sketch.js
@@ -1,13 +1,5 @@
-function setup() {
-  createCanvas(640, 480);
+function addOrange(word) {
+	var x = word + “Orange”;
+	return x;
 }
 
-function draw() {
-  if (mouseIsPressed) {
-    fill(random(254));
-  } else {
-    fill(random(254),random(254),random(254));
-  }
-  ellipse(mouseX, mouseY, 80, 80);
-  
-}
