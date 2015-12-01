76967522f81dcf6fe87465be2cece1567d59ae33
Natalie Kaminsky <kaminskynat@gmail.com>
2015-11-30 23:56:31 -0500

    made wordBubble sprites
    made wordBubble sprites to have p5.play functionality

--- FinalProject/FinalProject.js
+++ FinalProject/FinalProject.js
@@ -1,77 +1,129 @@
 var wordBubbles = [];
 var mostrecentword = "";
 
+// var WordBubble = function (word) {
+// 	this.x = random(width);
+// 	this.y = random(height);
+// 	this.diameter = 80;
+// 	this.xspeed = random(1, 1.5);
+// 	this.yspeed = random(1, 1.5);
+// 	this.word = word;
+// 	this.backgroundColor = color(random(255),random(255));
+// 	// this.bubbleSprite = createSprite(WordBubble);
+// 	//see custom draw Sprite ex.
+
+	
+// 	this.move = function() {
+// 		this.x = this.x + this.xspeed;
+// 		this.y = this.y + this.yspeed;
+// 		if (this.y > height || this.y < 0) {
+// 			this.yspeed = - this.yspeed;
+// 		}
+// 		if (this.x > width || this.x < 0) {
+// 			this.xspeed = - this.xspeed;
+// 		}
+// 	}
+
+// 	this.display = function() {
+// 		fill(this.backgroundColor);
+// 		ellipse(this.x, this.y, this.diameter, this.diameter);
+
+// 		fill('black');
+// 		textAlign(CENTER);
+// 		text(this.word, this.x, this.y);
+// 	}
+// }
+
+// var BubblesCollide = function() {
+
+// }
+
+function popMode() {
+	console.log('Popped');
+}
+
+function setup() {
+	// inp = createInput('City');
+	// inp.position(100, 30);
+
+
+	var button = createButton('Pop!');
+  	button.position(250, 30);
+  	button.mousePressed(popMode);
+  
+   	createCanvas(windowWidth, windowHeight);
+   	
+	myRec.onResult = parseResult; // recognition callback
+	myRec.onError = handleError;
+	myRec.onEnd = recognitionEnded;
+	myRec.start();
+	
+}
+
+function draw() {
+	background('white');
+
+	// for (var i = 0; i < wordBubbles.length; i++) {
+	// 	wordBubbles[i].move();
+	// 	wordBubbles[i].display();
+	// }
+
+	for(var i=0; i<allSprites.length; i++) {
+	  var s = allSprites[i];
+	  if(s.position.x<0) {
+	    s.position.x = 1;
+	    s.velocity.x = abs(s.velocity.x);
+	  }
+	  
+	  if(s.position.x>windowWidth) {
+	    s.position.x = width-1;
+	    s.velocity.x = -abs(s.velocity.x);
+	    }
+	  
+	  if(s.position.y<0) {
+	    s.position.y = 1;
+	    s.velocity.y = abs(s.velocity.y);
+	  }
+	  
+	  if(s.position.y>windowHeight) {
+	    s.position.y = height-1;
+	    s.velocity.y = -abs(s.velocity.y);
+	    } 
+	  }
+	drawSprites();
+}
+
 var myRec = new p5.SpeechRec(); // new P5.SpeechRec object
 myRec.continuous = false; // do continuous recognition
 myRec.interimResults = false; // allow partial recognition (faster, less accurate)
 
 function parseResult() {
 	mostrecentword = myRec.resultString.split(' ').pop();
-	wordBubbles.push(new WordBubble(mostrecentword));
+
+	var wordBubbleSprite = createSprite(random(windowWidth),random(windowHeight), 80, 80);
+	wordBubbleSprite.word = mostrecentword;
+	wordBubbleSprite.draw = spriteDraw;
+	wordBubbleSprite.addSpeed(random(5),random(360));
+	wordBubbles.push(wordBubbleSprite);
+
+	// wordBubbles.push(new WordBubble(mostrecentword));
 
 	console.log(mostrecentword);	
 }
 
-var WordBubble = function (word) {
-	this.x = random(width);
-	this.y = random(height);
-	this.diameter = 80;
-	this.xspeed = random(1, 2);
-	this.yspeed = random(1, 2);
-	this.word = word;
-	this.backgroundColor = 	color(random(255),random(255));
+function spriteDraw () {
+	fill('Ivory');
+	ellipse(this.position.x, this.position.y, this.height, this.width);
 
-	
-	this.move = function() {
-		this.x = this.x + this.xspeed;
-		this.y = this.y + this.yspeed;
-		if (this.y > height || this.y < 0) {
-			this.yspeed = - this.yspeed;
-		}
-		if (this.x > width || this.x < 0) {
-			this.xspeed = - this.xspeed;
-		}
-	}
-
-	this.display = function() {
-		fill('Black');
-		textAlign(CENTER);
-		text(this.word, this.x, this.y);
-
-		fill(this.backgroundColor);
-		ellipse(this.x, this.y, this.diameter, this.diameter);
-
-	}
-}
-
-
-function setup() {
-   	createCanvas(windowWidth, windowHeight);
-   	
-	myRec.onResult = parseResult; // recognition callback
-	myRec.onError = handleError;
-	myRec.onEnd = recognitionEnded;
-	myRec.start(); // start engine
-	
-}
-
-function draw() {
-	background('white');
-	// textSize(20);
-	// textAlign(LEFT);
-	// text(word, 20, 20);
-
-	for (var i = 0; i < wordBubbles.length; i++) {
-		wordBubbles[i].move();
-		wordBubbles[i].display();
-
-	}
+	fill('black');
+	textAlign(CENTER);
+	text(this.word, this.position.x, this.position.y);
 }
 				
   // draw stuff here
 
 function recognitionEnded () {
-	mostrecentword = myRec.resultString.split(' ').pop();
+	mostrecentword = myRec.resultString;//.split(' ').pop();
 	console.log("Recognition Ended: " + mostrecentword);
 	myRec.start();
 }
--- FinalProject/sketch.js
+++ FinalProject/sketch.js
@@ -1,27 +1,37 @@
+var sprite;
+var angle;
+var speed;
+
 function setup() {
-   createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth,windowHeight);
+  sprite = createSprite(width/2, height/2, 50, 50);
+  speed = 1;
+  angle = 270;
 }
 
 function draw() {
-	noFill();
-	x1 = 85, x2 = 10, x3 = 90, x4 = 15;
-	y1 = 20, y2 = 10, y3 = 90, y4 = 80;
-	bezier(x1, y1, x2, y2, x3, y3, x4, y4);
-	fill(255);
-	steps = 10;
-	for (i = 0; i <= steps; i++) {
-  		t = i / steps;
-  		x = bezierPoint(x1, x2, x3, x4, t);
- 		y = bezierPoint(y1, y2, y3, y4, t);
-  		ellipse(x, y, 5, 5);
-}
-  // draw stuff here
- 
+  background(255,255,255);  
+  sprite.setSpeed(speed, angle);
+  drawSprites();
+  checkForWallCollisions();
+
 }
 
-// function mousePressed() {
-// 	// fill(0);
-// 	// ellipse(mouseX, mouseY, 20, 20);
-// }
+function checkForWallCollisions() {
+  if (sprite.position.x > width) {
+    angle = 180;
+  }
 
+  if (sprite.position.x < 0) {
+    angle = 0;
+  }
 
+  if (sprite.position.y > height) {
+    angle = 270;
+  }
+
+  if (sprite.position.y < 0) {
+    angle = 90;
+  }
+
+}
