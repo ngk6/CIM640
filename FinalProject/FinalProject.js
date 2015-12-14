4642743f0a0d2ac4e16c41c7bc9829ea863340c5
Natalie Kaminsky <kaminskynat@gmail.com>
2015-12-14 00:12:49 -0500

    updated buttons, color scheme
    updated buttons, color scheme

--- FinalProject/FinalProject.js
+++ FinalProject/FinalProject.js
@@ -1,6 +1,7 @@
 var mostrecentword = "";
 
 var fish;
+var foshy;
 
 var popModeOn = false;
 var bounceModeOn = false;
@@ -9,7 +10,16 @@ var partyModeOn = false;
 var wordBubbles;
 var spriteSize = 80;
 
+var startColor;
+var endColor;
+var partyColor;
+var counter;
+
 var bubbleSound;
+
+var popButton;
+var bounceButton;
+var partyButton;
 
 function preload() {
 	bubbleSound = loadSound('assets/bubbles.mp3');
@@ -32,19 +42,23 @@ function setup() {
 	
 	wordBubbles = new Group();
 
+	var startColor = color(random(0, 255),random(0, 255),random(0, 255));
+	var endColor = color(random(0, 255),random(0, 255),random(0, 255));
+	counter = 0;
+
 	fish = createSprite(random(0, width), random(0,height));
 	fish.addAnimation("normal","assets/fish.png");
 
 	var button = createButton('Pop');
-  	button.position(windowWidth/2 - 140, windowHeight - 80);
+  	button.position(windowWidth/2 - 140, windowHeight - 70);
   	button.mousePressed(popMode);
   	
-  	var button = createButton('Bounce');
-  	button.position(windowWidth/2 - 40, windowHeight - 80);
+	var button = createButton('Bounce');
+  	button.position(windowWidth/2 - 40, windowHeight - 70);
   	button.mousePressed(bounceMode);
 
-  	var button = createButton('Party');
-  	button.position(windowWidth/2 + 88, windowHeight - 80);
+	var button = createButton('Party');
+  	button.position(windowWidth/2 + 88, windowHeight - 70);
   	button.mousePressed(partyMode);
  
 	myRec.onResult = parseResult; // recognition callback
@@ -55,20 +69,28 @@ function setup() {
 }
 
 function draw() {
-	//background(179, 240, 255);
-	background(26, 211, 255);
+	//background(26, 211, 255); // medium turquise
+	background(77, 148, 255); // darker
 
-	fill(255, 219, 102); 	// orangey 
-	//fill(230, 172, 0); //browner
-	noStroke();
-	//rect(0, windowHeight/3*2, width, windowHeight/3);
+	stroke(230, 172, 0); // brown
+	strokeWeight(3);
+	fill(255, 219, 102); // orangey 
 	ellipse(windowWidth/2, windowHeight, width + 100, windowHeight/3);
 
 	fish.position.x = mouseX;
 	fish.position.y = mouseY;
+	fish.mass = 1;
+	fish.setCollider("circle", 0, 0, spriteSize/1.8);
+	//fish.rotateToDirection = true;
+	//rotate(radians(fish.getDirection()));
+
+	//foshy.draw = function() {
+	// 	 image(fish, this.deltaX*2,this.deltaY*2);
+	// }
 
 	if (bounceModeOn) {
 		wordBubbles.bounce(wordBubbles);
+		fish.displace(wordBubbles);
 	} 
 
 	for (var i = 0; i < allSprites.length; i++) {
@@ -78,17 +100,17 @@ function draw() {
 	    s.velocity.x = abs(s.velocity.x);
 	  }
 	  
-	  if(s.position.x>windowWidth) {
+	  if(s.position.x > windowWidth) {
 	    s.position.x = width-1;
 	    s.velocity.x = -abs(s.velocity.x);
 	    }
 	  
-	  if(s.position.y<0) {
+	  if(s.position.y < 0) {
 	    s.position.y = 1;
 	    s.velocity.y = abs(s.velocity.y);
 	  }
 	  
-	  if(s.position.y>windowHeight) {
+	  if(s.position.y > windowHeight) {
 	    s.position.y = height-1;
 	    s.velocity.y = -abs(s.velocity.y);
 	    } 
@@ -115,24 +137,21 @@ function parseResult() {
 	wordBubbleSprite.draw = function () {
 
 		if (partyModeOn) {
-			fish.rotation -= 10;
-    		this.rotation -= 10;
+    		//fish.attractionPoint(1, mouseX, mouseY);
+    		// this.color(partyColor);
     		fill(color(random(0, 255),random(0, 255),random(0, 255)));
+    		// startColor = color(random(0, 255),random(0, 255),random(0, 255)));
+    		// endColor = color(random(0, 255),random(0, 255),random(0, 255)));
+    		// counter = .01;
+    		// partyColor = (lerpColor(startColor, endColor, counter));
 		} else {
-			//fill(128, 224, 255, 90);
-			//fill(229, 250, 255, 98);
 			fill(179, 255, 255, 90);
-
 		}
 		
 		strokeWeight(2);
-		//stroke(128, 224, 255);
-		//stroke(0, 184, 230);
-		//stroke(0, 50, 102);
 		stroke(255);
 		ellipse(0, 0, spriteSize, spriteSize);
 
-		//fill(0, 76, 128);
 		fill(0, 50, 102);
 		noStroke();
 		textAlign(CENTER);
@@ -147,12 +166,20 @@ function parseResult() {
 		if (popModeOn) {
 			this.remove();
 			bubbleSound.play();
+	// 		popButton.removeClass('normal');
+	// 		popButton.addClass('pressed');
+	// 	} else {
+	// 		popButton.removeClass('pressed');
+	// 		popButton.addClass('normal');
 		}
-		
+	}
+
+	{
 	wordBubbleSprite.debug = false;
 	console.log(mostrecentword);	
 	}
 }
+
 
 function recognitionEnded () {
 	mostrecentword = myRec.resultString;//.split(' ').pop();
